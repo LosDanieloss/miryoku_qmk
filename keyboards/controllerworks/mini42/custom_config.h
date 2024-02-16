6dbb01d0c34b5ac6e8dae70736179d6b1b4485c4
Daniel Łojewski <danielojewski@gmail.com>
2024-02-16 23:59:11 +0100

    Create custom_config.h

---
+++ keyboards/controllerworks/mini42/custom_config.h
@@ -0,0 +1,31 @@
+// Copyright 2019 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#define XXX KC_NO
+
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(EXTRA,  "Extra") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun") \
+MIRYOKU_X(GAME,  "Game")
+
+#define MIRYOKU_LAYER_GAME \
+XXX,           XXX,              KC_W,           XXX,              XXX,           XXX,              XXX,   XXX,  XXX,    XXX,     \
+XXX,           KC_D,             KC_S,           KC_D,             XXX,           XXX,              XXX,   XXX,  XXX,    XXX,             \
+XXX,           XXX,              XXX,            XXX,              XXX,           XXX,              XXX,   XXX,  XXX,    XXX,              \
+XXX,           XXX,              XXX,            XXX,              XXX,           XXX,              XXX,   XXX,  XXX,    XXX
+
+#define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
+
+#define U_GAME   10
