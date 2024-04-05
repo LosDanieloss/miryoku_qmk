21d61fc64230f7dd06a6d04f804df6cbb13727a6
Daniel Łojewski <danielojewski@gmail.com>
2024-04-05 11:05:27 +0200

    Update custom_config.- added game layer

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -18,12 +18,19 @@ MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun") \
-MIRYOKU_X(RL,     "Rocket")
+MIRYOKU_X(RL,     "Rocket") \
+MIRYOKU_X(GAME,   "Game") 
 
 #define MIRYOKU_LAYER_RL \
-XXX,       KC_Q,    KC_W,  KC_E,     XXX,                   XXX,      XXX,       KC_UP,    XXX,         XXX,          \
-XXX,       KC_A,    KC_S,  KC_D,     XXX,                   XXX,      KC_LEFT,   KC_DOWN,  KC_RIGHT,    XXX,          \
-KC_LSFT,   XXX,     XXX,   XXX,      XXX,                   XXX,      XXX,       XXX,      XXX,         XXX,          \
-KC_LSFT,   XXX,     XXX,   KC_SPC,   KC_ESC,                KC_ENT,   KC_RSFT,   XXX,      XXX,         LSFT(KC_TAB)
- 
+XXX,       KC_Q,    KC_W,    KC_E,     XXX,                 XXX,      XXX,       KC_UP,    XXX,         XXX,          \
+XXX,       KC_A,    KC_S,    KC_D,     XXX,                 XXX,      KC_LEFT,   KC_DOWN,  KC_RIGHT,    XXX,          \
+KC_LSFT,   XXX,     XXX,     XXX,      XXX,                 XXX,      XXX,       XXX,      XXX,         LSFT(KC_TAB),          \
+XXX,       XXX,     KC_ESC,  KC_SPC,   XXX,                 KC_ENT,   KC_RSFT,   XXX,      XXX,         XXX
+
+#define MIRYOKU_LAYER_GAME \
+KC_Q,    KC_W,  KC_E,     KC_R,      KC_T                   KC_Y,      KC_U,      KC_I,     KC_O,       KC_P, \
+KC_A,    KC_S,  KC_D,     KC_F,      KC_G                   KC_H,      KC_J,      KC_J,     KC_L,       KC_LGUI(KC_LSFT(KC_S)), \
+KC_Z,    KC_X,  KC_C,     KC_V,      KC_B,                  KC_N,      KC_M,      XXX,      KC_PMNS,    KC_PPLS, \
+XXX,     XXX,   KC_ESC,   KC_LSFT,   KC_SPC,                KC_ENT,    KC_RSFT,   KC_LCTL,  XXX,        XXX
+
 #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
