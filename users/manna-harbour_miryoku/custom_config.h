19352a4755955425a2e98c46ebcb9f1b395c170a
Daniel Łojewski <danielojewski@gmail.com>
2024-04-05 11:40:32 +0200

    Update custom_config.h

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -29,7 +29,7 @@ XXX,       XXX,     KC_ESC,  KC_SPC,   XXX,                 KC_ENT,   KC_RSFT,  
 
 #define MIRYOKU_LAYER_GAME \
 KC_Q,    KC_W,  KC_E,     KC_R,      KC_T,                  KC_Y,      KC_U,      KC_I,     KC_O,       KC_P, \
-KC_A,    KC_S,  KC_D,     KC_F,      KC_G,                  KC_H,      KC_J,      KC_J,     KC_L,       KC_LGUI(KC_LSFT(KC_S)), \
+KC_A,    KC_S,  KC_D,     KC_F,      KC_G,                  KC_H,      KC_J,      KC_J,     KC_L,       LGUI(LSFT(KC_S)), \
 KC_Z,    KC_X,  KC_C,     KC_V,      KC_B,                  KC_N,      KC_M,      XXX,      KC_PMNS,    KC_PPLS, \
 XXX,     XXX,   KC_ESC,   KC_LSFT,   KC_SPC,                KC_ENT,    KC_RSFT,   KC_LCTL,  XXX,        XXX
 
